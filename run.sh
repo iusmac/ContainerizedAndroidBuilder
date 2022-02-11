b0743afb667f7c5689e6e5079fe29c55e8e10c78
iusmac <iusico.maxim@libero.it>
2022-02-11 21:05:12 +0100

    Print a note when Docker image is missing
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- run.sh
+++ run.sh
@@ -375,6 +375,9 @@ function logsMenu() {
 function containerQuery() {
     # Build image if does not exist
     if ! sudo docker inspect --type image "$__IMAGE_TAG__" &> /dev/null; then
+        printf "Note: Unable to find '%s' image. Start building...\n" "$__IMAGE_TAG__"
+        printf "This may take a while...\n\n"
+
         sudo DOCKER_BUILDKIT=1 docker build \
             --no-cache \
             --build-arg USER="${__USER_IDS__['name']}" \
