256c18b75bb39baecfb6d41bca31e4c35dca7ccd
iusmac <iusico.maxim@libero.it>
2022-03-26 12:27:15 +0100

    Bump __VERSION__ to 3.2 (A11)
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o pipefail
 
-readonly __VERSION__='3.1 (A11)'
+readonly __VERSION__='3.2 (A11)'
 readonly __IMAGE_VERSION__='2.0'
 __DIR__="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 readonly __DIR__
