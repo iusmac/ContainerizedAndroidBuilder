390ce41d96a2033ce94ce858e58f4560564582cb
iusmac <iusico.maxim@libero.it>
2023-01-02 13:39:58 +0100

    run: Add `DOCKER_BUILD_PATH` env variable
    Allow overriding build context path.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- run.sh
+++ run.sh
@@ -488,7 +488,8 @@ function buildImageIfNone() {
                 --build-arg EMAIL="${__ARGS__['email']}" \
                 --build-arg UID="${__USER_IDS__['uid']}" \
                 --build-arg GID="${__USER_IDS__['gid']}" \
-                --tag "$__IMAGE_TAG__" "$__DIR__"/Dockerfile/ || exit $?
+                --tag "$__IMAGE_TAG__" \
+                "${DOCKER_BUILD_PATH:-"$__DIR__"/Dockerfile/}" || exit $?
         else
             printf "Note: Unable to find '%s' image. Pulling from repository...\n" "$__IMAGE_TAG__" >&2
             sudo docker pull "$__IMAGE_TAG__" || exit $?
