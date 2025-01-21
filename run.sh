81463db5d008bde808cab696cb2a63734eab2cf0
iusmac <iusico.maxim@libero.it>
2025-01-21 12:33:35 +0100

    run: bump to `v1.2`
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o pipefail
 
-readonly __VERSION__='1.1'
+readonly __VERSION__='1.2'
 readonly __IMAGE_VERSION__='1.2'
 __DIR__="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 readonly __DIR__
